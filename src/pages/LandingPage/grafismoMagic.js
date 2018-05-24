4b61545ba9e3cfe06db6a48c2a7a2df52e0c4913
Guilherme Duarte <gui13dula@gmail.com>
2018-05-24 17:01:34 -0300

    Animation change, now things rotate

--- src/pages/LandingPage/grafismoMagic.js
+++ src/pages/LandingPage/grafismoMagic.js
@@ -11,35 +11,59 @@ var controller = new ScrollMagic.Controller();
 
 var timeLine = new TimelineMax();
 
-var b1 = new TweenMax.to('#bar-1', 1000, {
+var b1 = new TweenMax.to('#bar-1', 1, {
     transform: "translateY(-100px)"
 })
 
-var b2 = new TweenMax.to('#bar-2', 500, {
+var b2 = new TweenMax.to('#bar-2', 1, {
     transform: "translateY(50px)"
 })
 
-var b3 = new TweenMax.to('#bar-3', 1000, {
+var b3 = new TweenMax.to('#bar-3', 1, {
     transform: "translateY(200px)"
 })
 
-var b4 = new TweenMax.to('#bar-4', 1000, {
+var b4 = new TweenMax.to('#bar-4', 1, {
     transform: "translateY(-30px)"
 })
 
-var b4 = new TweenMax.to('#circle', 1000, {
-    transform: "rotate(45deg)"
+var b5 = new TweenMax.to('#bar-5', 1, {
+    transform: "translateX(-60px)"
+})
+
+var b6 = new TweenMax.to('.circle', 1, {
+    // rotation: 45,
+})
+
+var b7 = new TweenMax.to('#traco', 1, {
+    transform: "translateX(30px)"
+})
+
+var b8 = new TweenMax.to('#traco-rotate', 1, {
+})
+
+var b9 = new TweenMax.to('#cirle-2', 0.5, {
+    transform: "translateX(30px)"
+})
+
+var b10 = new TweenMax.to('.cls-10', 0, {
+    strokeDashoffset: 0
 })
 
 timeLine.add(b1);
 timeLine.add(b2);
 timeLine.add(b3);
 timeLine.add(b4);
-timeLine.add(b4);
+timeLine.add(b5);
+timeLine.add(b6);
+timeLine.add(b7);
+timeLine.add(b8);
+timeLine.add(b9);
+timeLine.add(b10);
 
 var scene = new ScrollMagic.Scene({
     triggerElement: ".sobre",
-    duration: "250vh",
+    duration: "1000vh",
     offset: "-450vh"
 })
 .setPin(".grafismo *")
